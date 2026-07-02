734e05efa238965088dd35f65191360f83df11f3
openjarv <jarvis@acrossthewire.co.uk>
2026-07-02 23:28:10 +0100

    fix: reference version.py from __init__.py, remove stale admin.py

--- netbox_circuit_maintenance/__init__.py
+++ netbox_circuit_maintenance/__init__.py
@@ -1,11 +1,13 @@
 from netbox.plugins import PluginConfig
 
+from .version import __version__
+
 
 class CircuitMaintenanceConfig(PluginConfig):
     name = "netbox_circuit_maintenance"
     verbose_name = "Circuit Maintenance"
     description = "Track planned circuit maintenance windows in NetBox"
-    version = "0.1.0"
+    version = __version__
     base_url = "circuit-maintenance"
     min_version = "3.5.0"
 
